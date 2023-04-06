75cdb1e61ea61e40a5ac793fb3a781f9993e10d4
Kunal <kunal@joinwido.com>
2023-04-06 12:48:45 +0000

    get list from wido

--- src/App.js
+++ src/App.js
@@ -61,15 +61,15 @@ function App() {
 
   useEffect(() => {
     const fromTokens = [
-      // {
-      //   chainId: 42161,
-      //   address: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
-      //   protocol: "dex",
-      //   symbol: "ETH",
-      //   name: "ETH",
-      //   decimals: 18,
-      //   logoURI: "https://arbiscan.io/images/main/empty-token.png",
-      // },
+      {
+        chainId: 42161,
+        address: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+        protocol: "dex",
+        symbol: "ETH",
+        name: "ETH",
+        decimals: 18,
+        logoURI: "https://arbiscan.io/images/main/empty-token.png",
+      },
       {
         chainId: 42161,
         address: "0xfc5A1A6EB076a2C7aD06eD22C90d7E710E35ad0a",
@@ -81,22 +81,10 @@ function App() {
           "https://tokens.1inch.io/0xfc5a1a6eb076a2c7ad06ed22c90d7e710e35ad0a.png",
       },
     ];
-    const toTokens = [
-      {
-        chainId: 42161,
-        address: "0x854c4ee45b2379446c4bf1c2f872c26aa8d95d8d",
-        protocol: "uni-v3",
-        symbol: "UniV3 USDs/GMX",
-        name: "UniV3 USDs/GMX",
-        decimals: 18,
-        logoURI: "https://arbiscan.io/images/main/empty-token.png",
-      },
-    ];
     setFromTokens(fromTokens);
-    // setToTokens(fromTokens);
-    setToTokens(toTokens);
-    // getSupportedTokens({ chainId: [42161] }).then(setFromTokens);
-    // getSupportedTokens({ chainId: [42161] }).then(setToTokens);
+    getSupportedTokens({ chainId: [42161], protocol: ["uni-v3"] }).then(
+      setToTokens
+    );
   }, [setFromTokens, setToTokens]);
 
   return (
