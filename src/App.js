f26ae1bb8a3cc544b918716b775015edfee7a5bb
Kunal <kunal@joinwido.com>
2023-04-06 07:24:19 +0000

    wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { WidoWidget } from "wido-widget";
 import { getSupportedTokens } from "wido";
 import { useLocalApi } from "wido";
+import { useWeb3React } from "@web3-react/core";
+import { InjectedConnector } from "@web3-react/injected-connector";
+import { Web3Provider } from "@ethersproject/providers";
+
+export const injected = new InjectedConnector({});
 
 function App() {
   const [fromTokens, setFromTokens] = useState([]);
   const [toTokens, setToTokens] = useState([]);
+  const { library, activate, chainId, account } = useWeb3React();
+  const [ethProvider, setEthProvider] = useState();
+
   useLocalApi();
+
+  const handleMetamask = useCallback(async () => {
+    await activate(injected);
+  }, [activate]);
+
+  const handleConnectWalletClick = useCallback(
+    (chainId) => {
+      handleMetamask();
+    },
+    [handleMetamask]
+  );
+
+  useEffect(() => {
+    if (!library) {
+      setEthProvider(undefined);
+      return;
+    }
+    // every time account or chainId changes we need to re-create the provider
+    // for the widget to update with the proper address
+    setEthProvider(new Web3Provider(library));
+  }, [library, account, chainId, setEthProvider]);
 
   useEffect(() => {
     const fromTokens = [
+      // {
+      //   chainId: 42161,
+      //   address: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+      //   protocol: "dex",
+      //   symbol: "ETH",
+      //   name: "ETH",
+      //   decimals: 18,
+      //   logoURI: "https://arbiscan.io/images/main/empty-token.png",
+      // },
       {
         chainId: 42161,
-        address: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+        address: "0xfc5A1A6EB076a2C7aD06eD22C90d7E710E35ad0a",
         protocol: "dex",
-        symbol: "ETH",
-        name: "ETH",
+        symbol: "GMX",
+        name: "GMX",
         decimals: 18,
-        logoURI: "https://arbiscan.io/images/main/empty-token.png",
+        logoURI:
+          "https://tokens.1inch.io/0xfc5a1a6eb076a2c7ad06ed22c90d7e710e35ad0a.png",
       },
     ];
     const toTokens = [
@@ -44,10 +83,102 @@ function App() {
     <div className="App">
       <header className="App-header">
         <WidoWidget
+          onConnectWalletClick={handleConnectWalletClick}
+          ethProvider={ethProvider}
           fromTokens={fromTokens}
           toTokens={toTokens}
-          quoteApi={(request) => {
-            console.log(request);
+          quoteApi={async (request) => {
+            const endpoint = "quote_v2";
+            const paramsObj = {
+              from_chain_id: String(request.fromChainId),
+              from_token: request.fromToken,
+              to_chain_id: String(request.toChainId),
+              to_token: request.toToken,
+            };
+
+            if (request.slippagePercentage) {
+              paramsObj.slippage_percentage = String(
+                request.slippagePercentage
+              );
+            }
+            if (request.amount) {
+              paramsObj.amount = request.amount;
+            }
+            if (request.user) {
+              paramsObj.user = request.user;
+            }
+            if (request.partner) {
+              paramsObj.partner = request.partner;
+            }
+
+            paramsObj.lower_tick = String(-23000);
+            paramsObj.upper_tick = String(-20000);
+            if (request.tokenId) {
+              paramsObj.token_id = request.tokenId;
+            }
+            if (request.recipient) {
+              paramsObj.recipient = request.recipient;
+            }
+
+            const params = new URLSearchParams(paramsObj);
+            const url = `http://localhost:8080/${endpoint}?${params}`;
+
+            const res = await fetch(url);
+
+            if (!res.ok) {
+              // throw WidoError.from_api_response(await res.json());
+              throw new Error("API error");
+            }
+
+            const body = await res.json();
+            const { is_supported, steps, steps_count } = body;
+
+            const baseQuoteResult = {
+              isSupported: is_supported,
+              steps,
+              stepsCount: steps_count,
+            };
+
+            if (!request.amount && !request.user) {
+              return baseQuoteResult;
+            }
+
+            const {
+              price,
+              min_price,
+              from_token_usd_price,
+              from_token_amount,
+              from_token_amount_usd_value,
+              to_token_usd_price,
+              to_token_amount,
+              to_token_amount_usd_value,
+              expected_slippage,
+              min_to_token_amount,
+              from,
+              to,
+              data,
+              value,
+              messages,
+            } = body;
+
+            return {
+              ...baseQuoteResult,
+              price,
+              minPrice: min_price,
+              fromTokenUsdPrice: from_token_usd_price,
+              fromTokenAmount: from_token_amount,
+              fromTokenAmountUsdValue: from_token_amount_usd_value,
+              toTokenUsdPrice: to_token_usd_price,
+              toTokenAmount: to_token_amount,
+              toTokenAmountUsdValue: to_token_amount_usd_value,
+              expectedSlippage: expected_slippage,
+              minToTokenAmount: min_to_token_amount,
+              from,
+              to,
+              data,
+              value,
+              messages,
+            };
           }}
         />
       </header>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { sendToVercelAnalytics } from "./vitals";
+import { Web3ReactProvider } from "@web3-react/core";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Web3ReactProvider getLibrary={(lib) => lib}>
+      <App />
+    </Web3ReactProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 reportWebVitals(sendToVercelAnalytics);
